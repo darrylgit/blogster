6091b2eed74420ad95d83e0d98bda4bdfb1a2012
darrylgit <darrylsmithup@gmail.com>
2020-04-20 21:17:04 -0400

    Reconfigure MongoDB

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const keys = require('./config/keys');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 
-require('./models/User');
-require('./models/Blog');
-require('./services/passport');
+require("./models/User");
+require("./models/Blog");
+require("./services/passport");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -24,15 +24,15 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
-require('./routes/blogRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/blogRoutes")(app);
 
-if (['production'].includes(process.env.NODE_ENV)) {
-  app.use(express.static('client/build'));
+if (["production"].includes(process.env.NODE_ENV)) {
+  app.use(express.static("client/build"));
 
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("client", "build", "index.html"));
   });
 }
 
