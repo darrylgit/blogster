d8517bc4791d9ea3660a7e2e79a10c4bc6e51fcb
darrylgit <darrylsmithup@gmail.com>
2020-04-21 18:40:50 -0400

    Create unique keys for Redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,28 +14,8 @@ module.exports = app => {
   });
 
   app.get("/api/blogs", requireLogin, async (req, res) => {
-    const redis = require("redis");
-    const redisUrl = "redis://127.0.0.1:6379";
-    const client = redis.createClient(redisUrl);
-    const util = require("util");
-
-    client.get = util.promisify(client.get);
-
-    // Do we have any cached data in redis related to this query?
-    const cachedBlogs = await client.get(req.user.id);
-
-    // If yes, then respond to the request and return
-    if (cachedBlogs) {
-      return res.send(JSON.parse(cachedBlogs));
-    }
-
-    // If no, we respond to request and update cache
-
     const blogs = await Blog.find({ _user: req.user.id });
-
     res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post("/api/blogs", requireLogin, async (req, res) => {
--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,10 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
   console.log("IM ABOUT TO RUN A QUERY");
+
+  const key = { ...this.getQuery(), collection: this.mongooseCollection.name };
+
+  console.log(key);
+
   return exec.apply(this, arguments);
 };
