aa09c8248fa77cca6ec5952db7f1a3b376dde91e
darrylgit <darrylsmithup@gmail.com>
2020-04-23 18:07:36 -0400

    Programmatically close browser windows after tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -6,9 +6,12 @@ beforeEach(async () => {
   browser = await puppeteer.launch({
     headless: false
   });
-
   page = await browser.newPage();
   await page.goto("localhost:3000");
+});
+
+afterEach(async () => {
+  await browser.close();
 });
 
 it("can launch a browser", async () => {
