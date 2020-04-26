4730438c250955e36f1077da14ede286d817c52c
darrylgit <darrylsmithup@gmail.com>
2020-04-26 15:34:59 -0400

    Continue configuring CI server

---
+++ config/ci.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    "366657791313-gev08p00fm0r9hinc6vl61ar2b6cn521.apps.googleusercontent.com",
+  googleClientSecret: "lwZOIlsrk3skGAXGYrP9-NgT",
+  mongoUsername: "signiorgratiano",
+  mongoPassword: "yTQKxmcMSOKyFS9j",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379"
+};
