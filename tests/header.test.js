f2a3854cd223df91737612498afdc348f062b36d
darrylgit <darrylsmithup@gmail.com>
2020-04-26 09:49:20 -0400

    Abstract login logic into new login method on Page class

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,3 @@
-const sessionFactory = require("./factories/sessionFactory");
-const userFactory = require("./factories/userFactory");
 const Page = require("./helpers/page");
 
 // Set-up / Take-down
@@ -30,13 +28,7 @@ it("has a button that kicks user into OAuth flow", async () => {
 });
 
 it("shows logout button when user is signed in", async () => {
-  const user = await userFactory();
-  const { session, sig } = sessionFactory(user);
-
-  await page.setCookie({ name: "session", value: session });
-  await page.setCookie({ name: "session.sig", value: sig });
-  await page.goto("localhost:3000");
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login();
 
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
 
--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,4 +1,6 @@
 const puppeteer = require("puppeteer");
+const sessionFactory = require("../factories/sessionFactory");
+const userFactory = require("../factories/userFactory");
 
 class CustomPage {
   static async build() {
@@ -11,13 +13,23 @@ class CustomPage {
 
     return new Proxy(customPage, {
       get: function (target, property) {
-        return customPage[property] || page[property] || browser[property];
+        return customPage[property] || browser[property] || page[property];
       }
     });
   }
 
   constructor(page) {
     this.page = page;
+  }
+
+  async login() {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+
+    await this.page.setCookie({ name: "session", value: session });
+    await this.page.setCookie({ name: "session.sig", value: sig });
+    await this.page.goto("localhost:3000");
+    await this.page.waitFor('a[href="/auth/logout"]');
   }
 }
 
