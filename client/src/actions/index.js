281b0398d808330e5eb085164f647dedce8be0c5
darrylgit <darrylsmithup@gmail.com>
2020-04-28 13:46:30 -0400

    Add new api call to actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,27 +1,27 @@
-import axios from 'axios';
-import { FETCH_USER, FETCH_BLOGS, FETCH_BLOG } from './types';
+import axios from "axios";
+import { FETCH_USER, FETCH_BLOGS, FETCH_BLOG } from "./types";
 
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user');
+  const res = await axios.get("/api/current_user");
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = token => async dispatch => {
-  const res = await axios.post('/api/stripe', token);
+  const res = await axios.post("/api/stripe", token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const submitBlog = (values, history) => async dispatch => {
-  const res = await axios.post('/api/blogs', values);
+  const res = await axios.post("/api/blogs", values);
 
-  history.push('/blogs');
+  history.push("/blogs");
   dispatch({ type: FETCH_BLOG, payload: res.data });
 };
 
 export const fetchBlogs = () => async dispatch => {
-  const res = await axios.get('/api/blogs');
+  const res = await axios.get("/api/blogs");
 
   dispatch({ type: FETCH_BLOGS, payload: res.data });
 };
