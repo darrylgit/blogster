aa7f9128a36e9505d722f180814fdadc5763ea59
darrylgit <darrylsmithup@gmail.com>
2020-04-28 14:25:44 -0400

    Incorporate AWS SDK into route handler

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,5 +1,7 @@
 const AWS = require("aws-sdk");
+const uuid = require("uuid/v1");
 const keys = require("../config/keys");
+const requireLogin = require("../middlewares/requireLogin");
 
 const s3 = new AWS.S3({
   accessKeyId: keys.accessKeyId,
@@ -7,5 +9,17 @@ const s3 = new AWS.S3({
 });
 
 module.exports = app => {
-  app.get("/api/upload", (req, res) => {});
+  app.get("/api/upload", requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+
+    s3.getSignedUrl(
+      "putObject",
+      {
+        Bucket: "blogster-bucket-djw",
+        ContentType: "jpeg",
+        Key: key
+      },
+      (err, url) => res.send({ key, url })
+    );
+  });
 };
